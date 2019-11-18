6200bda6015b8c707ea514bbdf32fabb3d676dee
SlimSoftware <markenco512@gmail.com>
2019-11-18 11:20:17 +0100

    Restore old Development branch

---
+++ LerenTypen/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LerenTypen
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LerenTypen/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace LerenTypen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TestOverviewPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new TestOverviewPage(), testOverviewPageButton);
+        }
+
+        private void TrendingTestsPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new TrendingTestsPage(), trendingTestsPageButton);
+        }
+
+        private void TipPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new TipPage(), tipPageButton);
+        }
+
+        private void LeaderboardPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new LeaderboardPage(), leaderboardPageButton);
+        }
+     
+        private void LoginPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(new LoginPage(), loginPageButton);
+        }
+
+        /// <summary>
+        /// Changes the page to the specified page if this page is not 
+        /// already open and updates the menu buttons accordingly
+        /// </summary>
+        /// <param name="pageToChangeTo"></param>
+        /// <param name="pageToggleButton"></param>
+        private void ChangePage(Page pageToChangeTo, ToggleButton pageToggleButton)
+        {
+            if (frame.Content != pageToChangeTo)
+            {
+                frame.Navigate(pageToChangeTo);
+                SwitchMenuButtons(pageToggleButton);
+            }           
+        }
+
+        /// <summary>
+        /// Checks the specified button and unchecks all other buttons of the menu
+        /// </summary>
+        /// <param name="buttonToSwitchTo">The ToggleButton to check</param>
+        private void SwitchMenuButtons(ToggleButton buttonToSwitchTo)
+        {
+            testOverviewPageButton.IsChecked = false;
+            trendingTestsPageButton.IsChecked = false;
+            tipPageButton.IsChecked = false;
+            leaderboardPageButton.IsChecked = false;
+            loginPageButton.IsChecked = false;
+
+            buttonToSwitchTo.IsChecked = true;
+        }
+    }
+}
