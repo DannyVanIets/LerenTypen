7eb9d39451ee9e6ed705c84f0bb3294330e4a1a0
SlimSoftware <slimsoftware512@gmail.com>
2019-11-21 10:41:37 +0100

    Add timesMade and highscore columns to tests table

--- mysql_database/quicklylearningtyping.sql
+++ mysql_database/quicklylearningtyping.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 20 nov 2019 om 10:39
--- Serverversie: 10.1.37-MariaDB
--- PHP-versie: 7.3.0
+-- Gegenereerd op: 21 nov 2019 om 10:40
+-- Serverversie: 10.1.35-MariaDB
+-- PHP-versie: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -120,15 +120,17 @@ CREATE TABLE `tests` (
   `testDifficulty` tinyint(1) NOT NULL COMMENT '0 = makkelijk, 1 = middel, 2 = moeilijk',
   `createDate` date NOT NULL,
   `isPrivate` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = nee, publiek, 1 = ja, private',
-  `archived` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = false, niet gearchiveerd, 1 = true, gearchiveerd'
+  `archived` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 = false, niet gearchiveerd, 1 = true, gearchiveerd',
+  `timesMade` int(11) DEFAULT '0',
+  `highscore` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tests`
 --
 
-INSERT INTO `tests` (`testID`, `accountID`, `testName`, `testType`, `testDifficulty`, `createDate`, `isPrivate`, `archived`) VALUES
-(1, 1, 'Leren oefenen met namen!', 0, 0, '0000-00-00', 0, 0);
+INSERT INTO `tests` (`testID`, `accountID`, `testName`, `testType`, `testDifficulty`, `createDate`, `isPrivate`, `archived`, `timesMade`, `highscore`) VALUES
+(1, 1, 'Leren oefenen met namen!', 0, 0, '0000-00-00', 0, 0, 0, 0);
 
 --
 -- Indexen voor geëxporteerde tabellen
