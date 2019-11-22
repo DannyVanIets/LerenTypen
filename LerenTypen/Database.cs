c943d78e332ed501a3d6198d18cd047824b2d507
SlimSoftware <markenco512@gmail.com>
2019-11-22 12:09:13 +0100

    Migrate Database class to MS SQL

--- LerenTypen/Database.cs
+++ LerenTypen/Database.cs
@@ -1,75 +1,33 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
+﻿using System.Data.SqlClient;
 using System.Text;
-using System.Windows;
 
 namespace LerenTypen
 {
     static class Database
     {
-        private static MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder()
-        {
-            Server = "localhost",
-            UserID = "root",
-            Password = "",
-            Database = "quicklylearningtyping"
-        };
-
-        private static string connectionString = "Server=localhost;Database=quicklylearningtyping;Uid=root;";
-        /// <summary>
-        /// Method for adding tests to database. 
-        /// </summary>
-        public static void TestQuery()
-        {
-            try
-            {
-                using (MySqlConnection connection = new MySqlConnection(connectionString))
-                {
-                    connection.Open();
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append("INSERT INTO `testcontent` (`testContentID`, `testID`, `content`) VALUES (NULL, '1', 'Klaas-jan');");
-                    string MySql = sb.ToString();
-
-                    using (MySqlCommand command = new MySqlCommand(MySql, connection))
-                    {
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            //while (reader.Read())
-                            //{
-                                    
-                            //}
-                        }
-                    }
-                }
-            }
-            catch (MySqlException e)
-            {
-                System.Console.WriteLine(e.Message);
-            }
-        }
+        private static string connectionString = "Data Source=127.0.0.1,1433;User Id=qlt;Password=MvBg2T-{K[Vh;Database=quicklylearningtyping;";
 
         //Database query used by login. We're gonna check if the username and hashedpassword match any existing data. If it does, we will return the accountID.
         public static int GetAccountIDForLogin(string accountUsername, string password)
         {
             try
             {
-                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
                     StringBuilder sb = new StringBuilder();
 
                     // We only return the accountID, in case the logged in user's account information changes. That way nothing that may change is stored. ID's will always stay the same. We also check if the account is not archived.
-                    sb.Append($"SELECT `accountID` FROM accounts WHERE accountUsername = @accountusername AND accountPassword = @accountpassword AND archived = 0;");
+                    sb.Append($"SELECT accountID FROM accounts WHERE accountUsername = @accountusername AND accountPassword = @accountpassword AND archived = 0;");
 
-                    string MySql = sb.ToString();
+                    string Sql = sb.ToString();
 
-                    using (MySqlCommand command = new MySqlCommand(MySql, connection))
+                    using (SqlCommand command = new SqlCommand(Sql, connection))
                     {
                         command.Parameters.AddWithValue("@accountusername", accountUsername);
                         command.Parameters.AddWithValue("@accountpassword", password);
 
-                        using (MySqlDataReader reader = command.ExecuteReader())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
@@ -79,7 +37,7 @@ namespace LerenTypen
                     }
                 }
             }
-            catch (MySqlException e)
+            catch (SqlException e)
             {
                 System.Console.WriteLine(e.Message);
             }
