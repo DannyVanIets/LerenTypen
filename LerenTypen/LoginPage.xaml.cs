cd7b14d490c8e3594e45054c3e043c1569fe6bc5
Tristan <Tristan15015@gmail.com>
2019-11-22 10:17:59 +0100

    Geboortedatum schermontwerp aangepast + min/max datum

--- LerenTypen/LoginPage.xaml.cs
+++ LerenTypen/LoginPage.xaml.cs
@@ -58,7 +58,7 @@ namespace LerenTypen
             {  
                 string hashedpw = ComputeSha256Hash(password.Password);
                 Database.Registrer(username.Text, hashedpw.ToString(), birthdate.SelectedDate.Value.Date , firstname.Text, lastname.Text, securityvraag.Text, securityans.Text);
-                MessageBox.Show("Er is succesvol geregistreerd!" , "Succesvol Geregistreerd!");
+                MessageBox.Show("U bent succesvol ingelogd!"+"\n"+"U wordt nu doorgestuurd naar de homepagina." , "Succes");
                 username.Text = string.Empty; lastname.Text = string.Empty;
                 firstname.Text = string.Empty; password.Password = string.Empty;
                 passwordherh.Password = string.Empty; birthdate.Text = string.Empty; securityans.Text = string.Empty;
@@ -81,6 +81,7 @@ namespace LerenTypen
             string securtiyvraag = securityvraag.Text;
             string securityans = securityvraag.Text;
         }
+        
         
         private void CheckBox_unChecked(object sender, System.Windows.RoutedEventArgs e)
         {
