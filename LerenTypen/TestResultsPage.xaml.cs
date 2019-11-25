b670928e14da3fae25839bee79f4eaa7a35e2e5d
HugoRouwenhorst <s1103724@student.windesheim.nl>
2019-11-25 13:43:18 -0800

    able to show only wrong answers

--- LerenTypen/TestResultsPage.xaml.cs
+++ LerenTypen/TestResultsPage.xaml.cs
@@ -11,29 +11,51 @@ namespace LerenTypen
         private int testID;
         private MainWindow m;
         private Dictionary<int, string> wrongAnswers;
+        private List<string> rightAnswers;
+        private List<string> lines;
         public TestResultsPage(int testID, MainWindow m, Dictionary<int, string> wrongAnswers, List<string> lines, List<string> rightAnswers)
         {
             InitializeComponent();
             this.testID = testID;
             this.m = m;
             this.wrongAnswers = wrongAnswers;
+            this.rightAnswers = rightAnswers;
+            this.lines = lines;
+            FillAnswerList(false);
+
+
+        }
+
+        private void FillAnswerList(bool check)
+        {
+            AnswersLv.Items.Clear();
 
             foreach (KeyValuePair<int, string> answer in wrongAnswers)
             {
                 AnswersLv.Items.Add($"{answer.Value} \nJuiste antwoord: {lines[answer.Key]}");
             }
-            foreach (string answer in rightAnswers)
+            if (!check)
             {
-                AnswersLv.Items.Add($"{answer} \nGoed gedaan!");
+                foreach (string answer in rightAnswers)
+                {
+                    AnswersLv.Items.Add($"{answer} \nGoed gedaan!");
+                }
             }
-
-
         }
 
         private void RestartButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             m.frame.Navigate(new TestExercisePage(testID, m));
-        }       
-        
+        }
+
+        private void OnlyWrongCb_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FillAnswerList(true);
+        }
+
+        private void OnlyWrongCb_Unchecked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FillAnswerList(false);
+        }
     }
 }
