904a8617ff8a664e96be59592a6e0c890e33e13f
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2019-11-25 10:08:11 +0100

    Revert "Merge branch 'mssql' into Development"
    This reverts commit 9e44d2a973a87c64145673037537375f234a2646, reversing
    changes made to 5e638542402038506ca7242ef026d0e2fb09a1bd.

--- LerenTypen/Database.cs
+++ LerenTypen/Database.cs
@@ -1,15 +1,54 @@
-using Microsoft.OData.Edm;
+﻿using Microsoft.OData.Edm;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Text;
+using System.Windows;
 
 namespace LerenTypen
 {
     static class Database
     {
-        private static string connectionString = "Data Source=127.0.0.1,1433;User Id=qlt;Password=MvBg2T-{K[Vh;Database=quicklylearningtyping;";
-      
+        private static MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder()
+        {
+            Server = "localhost",
+            UserID = "root",
+            Password = "",
+            Database = "quicklylearningtyping"
+        };
+
+        private static string connectionString = "Server=localhost;Database=quicklylearningtyping;Uid=root;";
+        /// <summary>
+        /// Method for adding tests to database. 
+        /// </summary>
+        public static void TestQuery()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("INSERT INTO `testcontent` (`testContentID`, `testID`, `content`) VALUES (NULL, '1', 'Klaas-jan');");
+                    string MySql = sb.ToString();
+
+                    using (MySqlCommand command = new MySqlCommand(MySql, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            //while (reader.Read())
+                            //{
+                                    
+                            //}
+                        }
+                    }
+                }
+            }
+            catch (MySqlException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
         public static bool UserExists(string user)
         {
             try
@@ -75,29 +114,30 @@ namespace LerenTypen
                 Console.WriteLine(e);
             }
         }
-        
+
         //Database query used by login. We're gonna check if the username and hashedpassword match any existing data. If it does, we will return the accountID.
         public static int GetAccountIDForLogin(string accountUsername, string password)
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
 
                     connection.Open();
                     StringBuilder sb = new StringBuilder();
 
                     // We only return the accountID, in case the logged in user's account information changes. That way nothing that may change is stored. ID's will always stay the same. We also check if the account is not archived.
-                    sb.Append($"SELECT accountID FROM accounts WHERE accountUsername = @accountusername AND accountPassword = @accountpassword AND archived = 0;");
+                    sb.Append($"SELECT `accountID` FROM accounts WHERE accountUsername = @accountusername AND accountPassword = @accountpassword AND archived = 0;");
 
-                    string Sql = sb.ToString();
+                    string MySql = sb.ToString();
 
-                    using (SqlCommand command = new SqlCommand(Sql, connection))
+
+                    using (MySqlCommand command = new MySqlCommand(MySql, connection))
                     {
                         command.Parameters.AddWithValue("@accountusername", accountUsername);
                         command.Parameters.AddWithValue("@accountpassword", password);
 
-                        using (SqlDataReader reader = command.ExecuteReader())
+                        using (MySqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
@@ -107,7 +147,7 @@ namespace LerenTypen
                     }
                 }
             }
-            catch (SqlException e)
+            catch (MySqlException e)
             {
                 System.Console.WriteLine(e.Message);
             }
