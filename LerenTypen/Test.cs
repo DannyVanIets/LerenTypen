8c5c88c285caba2ca13ffbb4884d150e9ffae2ca
SlimSoftware <markenco512@gmail.com>
2019-11-25 14:23:09 +0100

    Add Test class

---
+++ LerenTypen/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LerenTypen
+{
+    class Test
+    {
+        public string Name { get; private set; }
+        public string AuthorUsername { get; private set; }
+        public int WordCount { get; private set; }
+        public int TimesMade { get; private set; }
+        public double AverageScore { get; private set; }
+        public double Highscore { get; private set; }
+        public int Version { get; private set; }
+        public int Difficulty { get; private set; }
+        public bool IsPrivate { get; private set; }
+    }
+}
