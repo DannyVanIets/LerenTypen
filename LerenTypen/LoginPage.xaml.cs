441f57dac94a6ee03ad8e7f9544685512956e905
SlimSoftware <markenco512@gmail.com>
2019-11-27 13:37:23 +0100

    Add enter key functionality for login

--- LerenTypen/LoginPage.xaml.cs
+++ LerenTypen/LoginPage.xaml.cs
@@ -152,5 +152,15 @@ namespace LerenTypen
             }
 
         }
+
+        private void Password_login_textbox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter && MainWindow.IsActive)
+            {
+                Login_click(sender, e);
+            }
+
+            e.Handled = true;
+        }
     }
 }
