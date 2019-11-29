7d76c517323feed6ce80003e7643c668596066b2
SlimSoftware <markenco512@gmail.com>
2019-11-29 10:12:55 +0100

    Rename Test class to TestTable

--- LerenTypen/TestTable.cs
+++ LerenTypen/TestTable.cs
@@ -1,36 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LerenTypen
+﻿namespace LerenTypen
 {
-    class Test
-    {
-        public string Name { get; private set; }
-        public List<string> Content { get; private set; }
-
-        public int WordCount { get; private set; }
-
-        public string Difficulty { get; private set; }
-
-        public bool isPrivate { get; private set; }
-
-        public Test(int wordCount, string difficulty)
-        {
-            this.WordCount = wordCount;
-            this.Difficulty = difficulty;
-        }
-        //public int GetTimesTaken(Account account)
-        //{
-        //    return null;
-        //}
-
-
-
-
-    }
     class TestTable
     {
         public int WPFNumber { get; set; }
@@ -38,17 +7,12 @@ namespace LerenTypen
         public int WPFTimesMade { get; set; }
         public int Highscore { get; set; }
         public int AmountOfWords { get; set; }
-
         public string Difficulty { get; set; }
         public string Uploader { get; set; }
-
         public int DifficultyBinder { get; set; }
-
         public bool IsPrivate { get; set; }
         public string Edit { get; set; } = "bewerken";
-
-        public string Delete { get; set; } = "verwijder";
-
+        public string Delete { get; set; } = "verwijderen";
         public int TestId { get; set; }
 
         public TestTable(int number, string name, int timesMade, int highscore, int amountOfWords, int testDifficulty, string uploader)
@@ -59,6 +23,7 @@ namespace LerenTypen
             this.Highscore = highscore;
             this.AmountOfWords = amountOfWords;
             this.Uploader = uploader;
+
             if (testDifficulty == 0)
             {
                 DifficultyBinder = 0;
@@ -84,6 +49,7 @@ namespace LerenTypen
             this.Highscore = highscore;
             this.AmountOfWords = amountOfWords;
             this.Uploader = uploader;
+
             if (testDifficulty == 0)
             {
                 DifficultyBinder = 0;
@@ -99,6 +65,7 @@ namespace LerenTypen
                 DifficultyBinder = 2;
                 Difficulty = "moeilijk";
             }
+
             if (isPrivate == 0)
             {
                 this.IsPrivate = false;
@@ -107,8 +74,8 @@ namespace LerenTypen
             {
                 this.IsPrivate = true;
             }
-            this.TestId = testId;
 
+            this.TestId = testId;
         }
 
     }
