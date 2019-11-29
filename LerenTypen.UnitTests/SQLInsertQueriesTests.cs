def862e968a2222739f09b34c4a1fc90eae914d4
HugoRouwenhorst <s1103724@student.windesheim.nl>
2019-11-29 05:43:01 -0800

    Moved queries

--- LerenTypen.UnitTests/SQLInsertQueriesTests.cs
+++ LerenTypen.UnitTests/SQLInsertQueriesTests.cs
@@ -5,44 +5,7 @@ namespace LerenTypen.UnitTests
     [TestFixture]
     class SQLInsertQueriesTests
     {
-        [Test]
-        [TestCase("Piet", false)]
-        [TestCase("H", true)]
-        [TestCase("", false)]
-        [TestCase("Danny", false)]
-        public void UserExists_Username_Bool(string username, bool result)
-        {
-            //Arrange
-            bool answer = false;
-            //Act
-            answer = Database.UserExists(username);
-            //Assert
-            Assert.AreEqual(result, answer);
-        }
 
-        [Test]
-        [TestCase("Piet", "Klaas", 0)]
-        [TestCase("", "", 0)]
-        [TestCase("H", "test123", 3)]
-        public void GetAccountIDForLogin_UsernamePassword_IntID(string username, string password, int result)
-        {
-            //Arrange
-            int answer = 0;
-            //Act
-            answer = Database.GetAccountIDForLogin(username, password);
-            //Assert
-            Assert.AreEqual(result, answer);
-        }
-
-        public void GetAccountUsername_intID_UserName(int ID, string result)
-        {
-            //Arrange
-            string answer = "";
-            //Act
-            answer = Database.GetAccountUsername(ID);
-            //Assert
-            Assert.AreEqual(result, answer);
-        }
 
 
     }
