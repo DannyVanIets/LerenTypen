42c6a9aed88a2cfc4c5783ffd04e25a7633c9a5c
HugoRouwenhorst <s1103724@student.windesheim.nl>
2019-11-29 05:11:08 -0800

    made database public and created first testCase

--- LerenTypen.UnitTests/SQLInsertQueriesTests.cs
+++ LerenTypen.UnitTests/SQLInsertQueriesTests.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LerenTypen;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace LerenTypen.UnitTests
 {
     [TestFixture]
     class SQLInsertQueriesTests
     {
+        [TestFixture]
+        public class StringCalculatorTests
+        {
+            [Test]
+            public static void Add_ThreeNumbers_ReturnsSum()
+            {
+                //Arrange
+
+                int result = 0;
+                //Act
+                result = calculator.Add("1,2,3");
+                //Assert
+                Assert.AreEqual(result, 6);
+            }
+
+        }
 
     }
 }
