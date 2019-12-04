da1f262a93b76df1411ac0ed7b8b8caa58a92f3e
SlimSoftware <markenco512@gmail.com>
2019-12-04 10:04:53 +0100

    Add unit test project

---
+++ LerenTypen.UnitTests/LoginTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using LerenTypen;
+using LerenTypen.Models;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ComputeSha256Hash_CheckHash_ReturnsSHA256Hash()
+        {
+            Converter converter = new Converter();
+            string result = converter.ComputeSha256Hash("test123");
+            Assert.AreEqual(result, "ecd71870d1963316a97e3ac3408c9835ad8cf0f3c1bc703527c30265534f75ae");
+        }
+    }
+}
---
+++ LerenTypen.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LerenTypen.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LerenTypen.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd07c811-c592-4c74-9c5d-b3d6d4205609")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
