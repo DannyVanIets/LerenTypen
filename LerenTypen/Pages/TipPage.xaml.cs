34a40af5c6d1c044d88b565fcc4a891ba71b6038
ThAYOVirus <bramherman9@gmail.com>
2019-12-11 15:57:08 +0100

    Update keyboard word clicker

--- LerenTypen/Pages/TipPage.xaml.cs
+++ LerenTypen/Pages/TipPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace LerenTypen
 {
@@ -20,6 +24,46 @@ namespace LerenTypen
         private void LoginRegisterButton_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.ChangePage(new LoginPage(MainWindow));
+        }
+
+        private void KeyUp_Clicker(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            List<string> allNames = new List<string>();
+            foreach (var item in TipPage_StackPanel_Keyboard_Row2.Children)
+            {
+                List<System.Windows.Shapes.Rectangle> allVars = new List<System.Windows.Shapes.Rectangle>();
+                allVars.Add((System.Windows.Shapes.Rectangle)item);
+                foreach (var i in allVars)
+                {
+                    allNames.Add(i.Name.Substring(19));
+
+                }
+            }
+            string check = Models.Tips.ShowKey(e, allNames);
+           // MessageBox.Show(check);
+            var k = "TipPage_Canvas_Key_" + check;
+            //MessageBox.Show(k);
+            foreach (var item in TipPage_StackPanel_Keyboard_Row2.Children)
+            {
+                List<System.Windows.Shapes.Rectangle> allVars = new List<System.Windows.Shapes.Rectangle>();
+                allVars.Add((System.Windows.Shapes.Rectangle)item);
+                foreach (var i in allVars)
+                {
+                    //MessageBox.Show("i is" + i);
+                    if (i.Name.Equals(k))
+                    {
+                        //MessageBox.Show("same");
+                        i.Visibility = Visibility.Hidden;
+                    }
+                    else
+                    {
+                        i.Visibility = Visibility.Visible;
+                    }
+
+                }
+            }
+            //MessageBox.Show(test);
+            //MessageBox.Show(TipPage_Canvas_Key_44.Name.Substring(19));
         }
     }
 }
