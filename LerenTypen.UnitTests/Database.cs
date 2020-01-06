b602aab9f0fff10abea2ee13f2c9819a3fb00841
SlimSoftware <slimsoftware512@gmail.com>
2020-01-06 12:37:20 +0100

    Remove unneeded code from unittest Database class

--- LerenTypen.UnitTests/Database.cs
+++ LerenTypen.UnitTests/Database.cs
@@ -6,40 +6,7 @@ using System.Data.SqlClient;
 namespace LerenTypen.UnitTests
 {
     class Database
-    {
-        public static string connectionString = "Data Source=127.0.0.1,1433;User Id=qlt;Password=MvBg2T-{K[Vh;Database=quicklylearningtyping;";
-        public static List<string> SelectQuery(string query)
-        {
-            List<string> result = new List<string>();
-            SqlConnection connection = new SqlConnection(Database.connectionString);
-            try
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            result.Add(reader.GetString(0));
-                        }
-                    }
-                }
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                connection.Close();
-                connection.Dispose();
-            }
-            return result;
-        }
-
+    { 
         public static void Connect()
         {
             SshClient client = new SshClient("145.44.233.184", "student", "toor2019");
