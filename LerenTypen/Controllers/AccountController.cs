5a0bdc418b716ba73eaecf79f7d1b3d35545a639
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2020-01-06 15:44:58 +0100

    Alle review unit test cases

--- LerenTypen/Controllers/AccountController.cs
+++ LerenTypen/Controllers/AccountController.cs
@@ -5,9 +5,9 @@ using System.Data.SqlClient;
 
 namespace LerenTypen.Controllers
 {
+    //In this controller we have all the Account queries we use in this appliciaton.
     public class AccountController
     {
-
         //Get all the account information, except for password.
         public static Account GetAllAccountInformation(int accountID)
         {
