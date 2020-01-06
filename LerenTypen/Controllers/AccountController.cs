63d810acd2a4ca269716ea581e0c62e88d7d7fd6
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2020-01-06 15:50:52 +0100

    Nog wat meer unit test cases toegevoegd voor accounts

--- LerenTypen/Controllers/AccountController.cs
+++ LerenTypen/Controllers/AccountController.cs
@@ -636,7 +636,6 @@ namespace LerenTypen.Controllers
             return true;
         }
 
-
         public static string GetAverageTestResultpercentage(string userName)
         {
             SqlConnection connection = new SqlConnection(Database.connectionString);
